7078829d1498491713f0f99ac165b3ae4acb05d7
dev <vladislav.mitroshyn@incode-it.com>
2021-07-26 14:19:01 +0300

    shortened type for InferActionsTypes

--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -20,8 +20,7 @@ let rootReducer = combineReducers({
 type RootReducer = typeof rootReducer;
 export type AppStateType = ReturnType<RootReducer>;
 
-type PropertiesTypes<T> = T extends { [key: string]: infer U } ? U : never;
-export type InferActionsTypes<T extends { [key: string]: (...args: any[]) => any }> = ReturnType<PropertiesTypes<T>>;
+export type InferActionsTypes<T> = T extends { [keys: string]: (...args: any[]) => infer U } ? U : never
 
 export type BaseThunkType<AT extends Action, R = Promise<void>> = ThunkAction<R, AppStateType, unknown, AT>;
 
